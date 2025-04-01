6e50cb32f303de2546b557869d46f26fad9c4ce1
Heinacat <tidehide@mail.ru>
2025-04-02 03:24:22 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//const firstName = 'George'
+//firstName = 'Ivan'
+//console.log('Hello', firstName)
+
+let firstName = 'George'
+firstName = 'Ivan'
+
+console.log('Hello', firstName)
+
+var lastName = 'George'
+lastName = 'Vlad'
+
+console.log(lastName)
